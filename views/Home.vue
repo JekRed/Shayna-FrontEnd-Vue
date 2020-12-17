6d12b21e89b81e81d8d235b55cffdcd5e3caaddc
Irfan Darmawan <74939210+JekRed@users.noreply.github.com>
2020-12-17 09:06:28 +0700

    vue js

---
+++ views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <BannerShayna />
+    <IGShayna />
+    <LogoShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import BannerShayna from "@/components/BannerShayna.vue";
+import IGShayna from "@/components/IGShayna.vue";
+import LogoShayna from "@/components/LogoShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    BannerShayna,
+    IGShayna,
+    LogoShayna,
+    FooterShayna,
+  },
+};
+</script>
